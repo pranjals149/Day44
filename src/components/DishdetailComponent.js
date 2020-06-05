a7c45784c93715efca699d6fff78b1892bf14b3d
Pranjal Srivastava <pranjals149@gmail.com>
2020-06-05 14:23:12 +0530

    CardList and Media - Day44

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+
+class Dishdetail extends Component{
+    
+    
+    constructor(props){
+        super(props);
+    }
+
+    renderComments(comments){
+        if(comments == null){
+            return(<div></div>);
+        }else{
+           return  comments.map((comment) => {
+                return(
+                    <div>
+                        <ul className="list-unstyled">
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p> -- {comment.author}, {comment.date}</p>
+                             </li>
+                        </ul>
+                    </div>
+                );
+            });
+        }
+    }
+    
+    render(){
+
+        if(this.props.dish != null){
+            return(
+                <div className="row">
+                    
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg src={this.props.dish.image} />
+                            <CardBody>
+                                <CardTitle>{this.props.dish.name}</CardTitle>
+                                <CardText>{this.props.dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        {this.renderComments(this.props.dish.comments)}
+                    </div>
+
+                </div>
+            );
+        }else{
+            return (<div></div>);
+        }
+
+    }
+}
+
+export default Dishdetail;
